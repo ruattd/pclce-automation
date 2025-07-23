19fc9c7d8cc049ab4a5b33dc21fa09ac5aa94be2
任天天 <ruattqwq@gmail.com>
2025-07-23 23:10:03 +0800

    feat(issue): 完成 issue 自动化

---
+++ src/targets/issue_comment.ts
@@ -0,0 +1,135 @@
+import { Context, ProbotOctokit } from "probot";
+import { Labels } from "../values";
+
+export default async function (context: Context<"issue_comment">) {
+    const payload = context.payload;
+    const issue = payload.issue;
+    const sender = payload.sender.login;
+    const comment = payload.comment;
+    console.info(`#${issue.number} comment: ${comment.id} ${payload.action} by '${sender}'`);
+    // process commands
+    const content = comment.body.trim();
+    if (!content.startsWith("/")) {
+        console.debug("Not a command, ignored");
+        return;
+    }
+    const commandLine = content.slice(1).trim();
+    console.info(`Processing command line: ${commandLine}`);
+    const split = commandLine.split(" ");
+    const command = split[0].toLowerCase();
+    const args = [];
+    let stringOpened = false;
+    if (split.length > 1) for (const s of split.slice(1)) if (s.length > 0) {
+        if (s.startsWith('"') && !stringOpened) {
+            if (s.endsWith('"')) {
+                args.push(s.slice(1, -1));
+            } else {
+                stringOpened = true;
+                args.push(s.slice(1));
+            }
+        } else if (s.endsWith('"') && stringOpened) {
+            stringOpened = false;
+            args[args.length - 1] += " " + s.slice(0, -1);
+        } else if (stringOpened) {
+            args[args.length - 1] += " " + s;
+        } else {
+            args.push(s);
+        }
+    }
+    console.info(`Result: ${command} ["${args.join('", "')}"]`);
+    // command logic
+    const octokit = context.octokit;
+    switch (command) {
+        case "ping":
+            await octokit.issues.createComment(context.issue({ body: `Pong! @${sender}` }));
+            break;
+        case "duplicate":
+            const pl = await octokit.repos.getCollaboratorPermissionLevel(context.repo({ username: sender }));
+            const permission = pl.data.permission
+            console.debug(`${sender}'s permission: ${permission}`)
+            if (permission === "none") {
+                console.warn("Permission denied");
+                break;
+            }
+            if (args.length < 1) {
+                console.warn("Invalid argument");
+                break;
+            }
+            const dup = Number.parseInt(args[0]);
+            if (Number.isNaN(dup)) {
+                console.warn(`${args[0]} is not a number`);
+                break;
+            }
+            await octokit.issues.createComment(context.issue({ body: `本 issue 与 #${dup} 重复，请参考原 issue 相关信息。` }));
+            console.info(`Closing issue as duplicate of #${dup}`);
+            await markIssueAsDuplicate(octokit, context.issue({ duplicate_of: dup }));
+            await octokit.issues.removeAllLabels(context.issue());
+            await octokit.issues.addLabels(context.issue({ labels: await context.label(Labels.duplicate) }));
+            break;
+        default:
+            console.warn(`Unknown command: ${command}`);
+    }
+}
+
+async function markIssueAsDuplicate(
+    octokit: ProbotOctokit,
+    data: { owner: string, repo: string, issue_number: number, duplicate_of: number },
+) {
+    const owner = data.owner;
+    const repo = data.repo;
+    const issueNumber = data.issue_number;
+    const duplicateOf = data.duplicate_of;
+    interface GetIdsResponse {
+        repository: {
+            targetIssue: { id: string };
+            canonicalIssue: { id: string };
+        };
+    }
+    const GET_IDS = `
+        query getIssueIds(
+            $owner: String!,
+            $repo: String!,
+            $issueNumber: Int!,
+            $duplicateOf: Int!
+        ) {
+            repository(owner: $owner, name: $repo) {
+                targetIssue: issue(number: $issueNumber) {
+                    id
+                }
+                canonicalIssue: issue(number: $duplicateOf) {
+                    id
+                }
+            }
+        }
+    `;
+    const {
+        repository: {
+            targetIssue: { id: targetIssueId },
+            canonicalIssue: { id: canonicalIssueId },
+        },
+    } = await octokit.graphql<GetIdsResponse>(GET_IDS, {
+        owner,
+        repo,
+        issueNumber,
+        duplicateOf
+    });
+    const CLOSE_AS_DUPLICATE = `
+        mutation closeAsDuplicate($input: CloseIssueInput!) {
+            closeIssue(input: $input) {
+                issue {
+                    id
+                    number
+                    state
+                    stateReason
+                }
+            }
+        }
+    `;
+    await octokit.graphql(CLOSE_AS_DUPLICATE, {
+        input: {
+            issueId: targetIssueId,
+            duplicateIssueId: canonicalIssueId,
+            stateReason: "DUPLICATE",
+        },
+    });
+}
