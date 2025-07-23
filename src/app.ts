85a79675619843eefa75075a925a1ea3b9e11c1c
任天天 <ruattqwq@gmail.com>
2025-07-23 13:44:51 +0800

    fix: 修复活动日志 undefined sender 导致的空引用异常

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ export default (app: Probot) => {
     app.log.info("Hello from PCL CE Automation");
     app.onAny(async (context) => {
         const payload = context.payload as any;
-        console.debug(`${context.name}.${payload.action} by '${payload.sender.login}' on '${payload.repository.full_name}'`);
+        console.debug(`${context.name}.${payload?.action} by '${payload?.sender?.login}' on '${payload?.repository?.full_name}'`);
     });
     // register event targets
     app.on("issues.opened", issuesOpened);
