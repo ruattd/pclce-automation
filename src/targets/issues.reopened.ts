b8025938cfdef648daf483b4816a1c9cfce5943c
任天天 <ruattqwq@gmail.com>
2025-07-24 12:45:58 +0800

    feat(issue): 自动移除 reopen issue 的 negative 标签

---
+++ src/targets/issues.reopened.ts
@@ -0,0 +1,25 @@
+import { Context } from "probot";
+import { Labels } from "../values";
+
+export default async function (context: Context<"issues.reopened">) {
+    const payload = context.payload;
+    const issue = payload.issue;
+    console.info(`#${issue.number} reopened: ${issue.title} [${issue.user.login}]`);
+    // check sender type
+    const sender = payload.sender;
+    if (sender.type !== "User") {
+        console.debug(`Ignored non-user sender: ${sender.login} (${sender.type})`);
+        return;
+    }
+    // remove not planned & duplicate label
+    const octokit = context.octokit;
+    const issueLabelIds = issue.labels?.map(l => l.id) || [];
+    const labelsToRemove = [];
+    for (const l of issueLabelIds)
+        if (Labels.isNotPlannedLabel(l) || Labels.isDuplicateLabel(l)) labelsToRemove.push(l);
+    if (labelsToRemove.length > 0) {
+        const labelNames = await context.label(...labelsToRemove);
+        console.info(`Removing label(s): ${labelNames.join(", ")}`);
+        for (const n of labelNames) await octokit.issues.removeLabel(context.issue({ name: n }));
+    }
+}
